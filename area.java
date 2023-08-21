204c189f4bbb3fd1ebcfe5291114c2b8c4418691
vigneshkumar06 <142786400+vigneshkumar06@users.noreply.github.com>
2023-08-21 09:05:51 +0000

    completed

---
+++ area.java
@@ -0,0 +1,10 @@
+class area{
+    public static void main(String args[])
+    {
+        double PI = 3.14159;
+        double radius =10;
+        double area=PI * radius*radius;
+        System.out.println("The area of the circle with radius "+ radius + " is: "+ area);
+
+    }
+}               
